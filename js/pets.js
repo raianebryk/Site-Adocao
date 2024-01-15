56a181536ce5127e976a32c0070585f9440d32d7
Raiane Bryk <raiane@email.com>
2024-01-15 15:00:12 -0300

    página inicial do site

---
+++ js/pets.js
@@ -0,0 +1,25 @@
+function getPets() {
+    const pets = [
+        {
+            dataChegada: '2024-01-15',
+            idade: '45 dias',
+            tamanho: 'médio',
+            cidade: 'Itajaí',
+            contato: '47999556677'
+        },
+        {
+            dataChegada: '2024-01-15',
+            idade: '45 dias',
+            tamanho: 'médio',
+            cidade: 'Itajaí',
+            contato: '47999556677'
+        },
+        {
+            dataChegada: '2024-01-15',
+            idade: '45 dias',
+            tamanho: 'médio',
+            cidade: 'Itajaí',
+            contato: '47999556677'
+        }
+    ]
+}
